e91d919b4406f75f59b3024f188e2620a1fdc6db
Yura <bloadevil@gmail.com>
2020-04-24 08:26:51 +0300

    Updates

---
+++ precache-manifest.d2e3e6df564a0725e6b86ab02721f144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85642d6f2e20f16b261775486f0500e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbfe6caafab8b57f2157",
+    "url": "/static/css/main.651d824e.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfe6caafab8b57f2157",
+    "url": "/static/js/main.45e2f3a3.chunk.js"
+  },
+  {
+    "revision": "ad591d0f4708f88e02d5",
+    "url": "/static/js/runtime-main.ca69820c.js"
+  }
+]);
